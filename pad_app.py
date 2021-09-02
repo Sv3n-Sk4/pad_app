b2ccd26de44a3a6f048926b5149dafd80d731561
Sv3n-Sk4 <62945591+Sv3n-Sk4@users.noreply.github.com>
2021-09-02 16:27:29 +0200

    Update pad_app.py

--- pad_app.py
+++ pad_app.py
@@ -354,12 +354,14 @@ if graph_display:
 #     print(source_code)
 #     components.html(source_code)
 
-    st.write("")
-    st.write("Analyse Lime du client : ") 
-    st.write("")           
-    
-    https://htmlpreview.github.io/?https://github.com/Sv3n-Sk4/pad_app/blob/main/Images/limeexport.html
-    
+#     st.write("")
+#     st.write("Analyse Lime du client : ") 
+#     st.write("")  
+      
+      st.write("")
+      st.write("Analyse Lime du client : ") 
+      st.write(https://htmlpreview.github.io/?https://github.com/Sv3n-Sk4/pad_app/blob/main/Images/limeexport.html)  
+          
     components.html("https://htmlpreview.github.io/?https://github.com/Sv3n-Sk4/pad_app/blob/main/Images/limeexport.html", width=200, height=200)
 
 #     HtmlFile = open("https://raw.githubusercontent.com/Sv3n-Sk4/pad_app/main/Images/limeexport.html", 'r', encoding='utf-8')
